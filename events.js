9b67ec6f127b201c5920bdc285838e21a013f5dd
WBCent <W_Beressi123@outlook.com>
2023-01-11 18:38:49 +0000

    Last commit of the day 11/01/2023

---
+++ events.js
@@ -0,0 +1,14 @@
+
+//Generating Random numbers for the Target
+function getRandomTargetInteger() {
+    Math.floor(Math.random() *1000) + 1;
+};
+
+//Generate Random Number for the Numbers
+function getFourRandomIntegers() {
+    
+}
+
+
+
+//Generate Results
